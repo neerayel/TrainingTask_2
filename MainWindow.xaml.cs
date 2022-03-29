73603ecdeb103a2d2dfeabbec333911331c7ab1f
neerayel <andrei11shum@mail.ru>
2022-03-29 18:40:51 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+
+namespace Lab_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OutputResult.Clear();
+
+            float number;
+            int grade;
+
+            if (float.TryParse(InputNumber.Text, out number) && int.TryParse(InputGrade.Text, out grade))
+            {
+                OutputResult.Text = string.Format("{0:f5}", MyMath.MyRoot(number, grade, 0.01F));
+            }
+            else
+            {
+                OutputResult.Text += "Поля Число и Степень должны содержать цифры";
+            }
+
+            if (float.TryParse(InputNumber.Text, out number))
+            {
+                int inpDecimal = Convert.ToInt32(float.Parse(InputNumber.Text));
+
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in System.Text.Encoding.Unicode.GetBytes(inpDecimal.ToString()))
+                    sb.Append(Convert.ToString(b, 2).PadLeft(8, '0')).Append(' ');
+
+                string binaryStr = sb.ToString();
+
+                OutputBytes.Text = binaryStr;
+            }
+        }
+
+        private void Number(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(InputNumber.Text))
+            {
+                NumberHelpText.Opacity = 0;
+                InputNumber.Opacity = 1;
+            }
+            else
+            {
+                NumberHelpText.Opacity = 1;
+                InputNumber.Opacity = 0.5;
+            }
+        }
+
+        private void Grade(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(InputGrade.Text))
+            {
+                GradeHelpText.Opacity = 0;
+                InputGrade.Opacity = 1;
+            }
+            else
+            {
+                GradeHelpText.Opacity = 1;
+                InputGrade.Opacity = 0.5;
+            }
+        }
+
+        private void Result(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(OutputResult.Text))
+            {
+                ResultHelpText.Opacity = 0;
+                OutputResult.Opacity = 1;
+            }
+            else
+            {
+                ResultHelpText.Opacity = 1;
+                OutputResult.Opacity = 0.5;
+            }
+
+            int grade;
+            float inp;
+
+            if(float.TryParse(InputNumber.Text, out inp) && int.TryParse(InputGrade.Text, out grade))
+            {
+                OutputComparison.Text = " Результат Math.Pow - " + string.Format("{0:f5}", Math.Pow(inp, 1F / grade));
+            }
+        }
+
+        private void Comparison(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(OutputComparison.Text))
+            {
+                ComparisonHelpText.Opacity = 0;
+                OutputComparison.Opacity = 1;
+            }
+            else
+            {
+                ComparisonHelpText.Opacity = 1;
+                OutputComparison.Opacity = 0.5;
+            }
+        }
+
+        private void Bytes(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(OutputBytes.Text))
+            {
+                BytesHelpText.Opacity = 0;
+                OutputBytes.Opacity = 1;
+            }
+            else
+            {
+                BytesHelpText.Opacity = 1;
+                OutputBytes.Opacity = 0.5;
+            }
+        }
+    }
+}
---
+++ MyMath.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Lab_2
+{
+    internal class MyMath
+    {
+        public static double MyPow(float number, int n)
+        {
+            double result = 1;
+
+            if (n > 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    result *= number;
+                }
+            }
+
+            return result;
+        }
+
+        public static float MyAbs(float number)
+        {
+            if (number < 0)
+            {
+                number *= -1;
+            }
+
+            return number;
+        }
+
+        public static float MyRoot(float number, int n, float eps)
+        {
+            float tempPreviousX = 0F;
+            float result = 1F;
+
+            while (MyAbs(result - tempPreviousX) > eps)
+            {
+                tempPreviousX = result;
+                result = (1F / n) * ((n - 1) * tempPreviousX + number / (float)MyPow(tempPreviousX, n - 1));
+            }
+
+            return result;
+        }
+    }
+}
